3a984afb711b3375f551cf80c5cc2815eee698e0
fabien <pinel.fabien@gmail.com>
2013-11-29 11:18:16 +0100

    revoir parser getadate

--- src/ui/Parser.java
+++ src/ui/Parser.java
@@ -52,13 +52,6 @@ public class Parser {
 		int year = Integer.parseInt(reponse.substring(6, reponse.length()));
 		int month = Integer.parseInt(reponse.substring(3, 4));
 		int day = Integer.parseInt(reponse.substring(0, 1));
-		GregorianCalendar laDate = new GregorianCalendar(year, month, day);
-		GregorianCalendar today = new GregorianCalendar();
-		if (day < 1 || day > 31 || month < 10 || month > 12 || year < 0
-				|| (laDate.compareTo(today) < 0)) {
-			System.out.println("Date incorrect");
-			return null;
-		}
-		return laDate;
+		return new GregorianCalendar(year, month, day);
 	}
 }
